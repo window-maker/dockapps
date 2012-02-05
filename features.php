81020ea46a7eee8c4e51b83c40625fc693eb2198
Carlos R. Mafra <crmafra@gmail.com>
2012-02-05 21:58:56 +0000

    Add features section
    And use a few wikipedia links in the text.

---
+++ features.php
@@ -0,0 +1,110 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta name="generator" content="HTML Tidy for Linux (vers 25 March 2009), see www.w3.org" />
+
+  <title>Window Maker: Features</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=us-ascii" />
+  <link href="title.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <?php
+            include("header.php");
+            include("dock.php");
+    ?>
+
+  <div id="inhalt">
+    <table width="880" border="0" cellpadding="1" cellspacing="1">
+      <tr>
+        <td content="content" colspan="2" valign="top">
+          <br />
+          <br />
+
+          <h3>Features</h3>
+
+          <p>We have already mentioned that Window Maker is a window manager. There are other window managers out there, such as
+          the infamous FVWM, Enlightenment, AfterStep, and the list goes on. All window managers perform the same basic tasks,
+          which are to create, place, keep track of, and draw borders around the windows on the screen. Beyond this, they can
+          become quite different in their various abilities.</p>
+
+          <p>Window Maker firmly adheres to the behavior and functionality of the NEXTSTEP&#8482; user interface. The developers
+          have put forth a great deal of effort in capturing the essense and beauty of the original design, and have incorporated
+          some new ideas of their own. This has always followed the philosophy of keeping to those features which fit well into
+          the overall design, while limiting the amount of "feature creep" that tends to bloat other window managers. A summary
+          of the main Window Maker features are presented below.</p>
+
+          <h3>Core (usability)</h3>
+
+          <ul>
+            <li>Almost complete <a href="http://en.wikipedia.org/wiki/Icccm">ICCCM</a> compliance</li>
+
+            <li>National language I18N support (over 11 locales)</li>
+
+            <li>Built-in icon dithering with support for 4bpp and 8bpp displays</li>
+
+            <li>Popup menus that support keyboard traversal, which can be "pinned" to the root window</li>
+
+            <li>Support for <a href="http://gnustep.org">GNUstep</a>, <a href="http://gnome.org">GNOME</a>, and <a href=
+            "http://kde.org">KDE</a> window hints to better integrate with those desktop environments</li>
+
+            <li>Support for Motif&#8482; and OPEN LOOK&#8482; window hints to better interface with applications based on those
+            toolkits</li>
+
+            <li>Built-in GUI configuration utility that eliminates the need to hand edit config files</li>
+
+            <li>Application <a href="http://en.wikipedia.org/wiki/Dock_(computing)">Dock</a> (similar to NEXTSTEP/MacOS X Dock)
+            that can be configured using drag and drop</li>
+
+            <li>Workspace Dock (aka Clip/Fiend) which is a workspace specific Dock extender</li>
+
+            <li>Support for rudimentary session management</li>
+
+            <li>Rudimentary support for the <a href="http://en.wikipedia.org/wiki/RandR">X RandR</a> extension</li>
+
+            <li>Support for <a href="http://en.wikipedia.org/wiki/Dockapps">dockapps</a> (equivalent of applets or epplets)</li>
+
+            <li>Ability to change all preferences and menus on-the-fly without having to restart the window manager</li>
+
+            <li>Support for multiple workspaces (aka "virtual desktops")</li>
+          </ul>
+
+          <h3>Extras (eye candy)</h3>
+
+          <ul>
+            <li>Built-in themes support</li>
+
+            <li>Over 13 types of window decorations, including custom defined</li>
+
+            <li>Support for XPM, PNG, JPEG, TIFF, GIF and PPM icons (no conversions with external programs) with an
+            alpha-channel</li>
+
+            <li>Support for setting the root window background (via the wmsetbg utility)</li>
+
+            <li>Optional superfluous animations, such as window shading, customizeable icon miniturization effects,
+            slide/scrolling menus, and much more</li>
+          </ul>
+
+          <p>Despite all of these features, Window Maker is not resource intensive and remains stable across many UNIX varients.
+          It is extremely flexible, and many options can be included or excluded at compile time. This means that you can easily
+          tailor Window Maker to meet your needs; whether you're a minimalist wanting to save resources, or an extremist that
+          likes to theme everything in sight.</p>
+          <!--    <p>Visit the links below to learn more about this versatile window manager.</p>
+
+<ul>
+<li><a href="/features/features.edit">General Overview</a></li>
+<li><a href="/features/features/configuration.edit">Configuration via WPrefs</a></li>
+<li><a href="/features/features/menu.edit">Advanced Menu System</a></li>
+<li><a href="/features/features/dock.edit">The Dock and Clip</a></li>
+<li><a href="/features/features/appearance.edit">Looks and Apperance</a></li>
+<li><a href="/features/features/performance.edit">Performance Comparison</a></li>
+</ul>
+-->
+          <br />
+          <br />
+          <br />
+        </td>
+      </tr>
+    </table>
+  </div>
+</body>
+</html>
--- home.php
+++ home.php
@@ -8,10 +8,10 @@
 </head>
 
 <body>
-<?php
-	include("header.php");
-	include("dock.php");
-?>
+  <?php
+          include("header.php");
+          include("dock.php");
+  ?>
 
   <div id="inhalt">
     <table width="880" border="0" cellpadding="1" cellspacing="1">
@@ -24,21 +24,22 @@
 
           <p><strong>Window Maker</strong> is an X11 window manager originally designed to provide integration support for the
           <a href="http://gnustep.org">GNUstep</a> Desktop Environment. In every way possible, it reproduces the elegant look and
-          feel of the NEXTSTEP user interface. It is fast, feature rich, easy to configure, and easy to use. It is also free
-          software, with contributions being made by programmers from around the world.</p>
+          feel of the <a href="http://en.wikipedia.org/wiki/NEXTSTEP">NEXTSTEP</a> user interface. It is fast, feature rich, easy
+          to configure, and easy to use. It is also free software, with contributions being made by programmers from around the
+          world.</p>
 
-          <p>Window Maker includes compatibility options which allow it to work with other popular desktop environments, namely
-          GNOME and KDE, and comes with a powerful GUI configuration editor, called WPrefs, which removes the need to edit
-          text-based config files by hand. It also supports running <a href="http://dockapps.windowmaker.org">dockapps</a> which
-          help on tasks such as monitoring system performance and battery usage, mounting devices or connecting to the internet
-          etc. Please see the features section for more specifics on what Window Maker can do, and how it compares with other
+          <p>Window Maker has a stable user interface since many years and comes with a powerful GUI configuration editor called
+          WPrefs, which removes the need to edit text-based config files by hand. It supports running <a href=
+          "http://dockapps.windowmaker.org">dockapps</a> which make everyday tasks such as monitoring system performance and
+          battery usage, mounting devices or connecting to the internet very easy. Please see the <a href=
+          "features.php">features</a> section for more specifics on what Window Maker can do, and how it compares with other
           popular window managers. A screenshot of a typical Window Maker workspace is <a href="wmaker-screenshot.png" target=
           "_blank">here</a>.</p>
 
           <h3>Latest stable version</h3>
 
-          <p>The latest stable version is 0.95.1, released on 29.01.2012. See the <a href="news.php">NEWS</a> section for more
-          information.</p>
+          <p>The latest <a href="http://repo.or.cz/w/wmaker-crm.git/shortlog/refs/heads/master">stable</a> version is 0.95.1,
+          released on 29.01.2012. See the <a href="news.php">NEWS</a> section for more information.</p>
         </td>
       </tr>
     </table>
