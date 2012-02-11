923e78fe758f096507a11244f33b726556b4b53c
Carlos R. Mafra <crmafra@gmail.com>
2012-02-11 22:36:25 +0000

    dev: Small updates

--- dev.php
+++ dev.php
@@ -19,35 +19,36 @@
 
           <h3>Source code versioning system</h3>
 
-          <p>The source code for Window Maker is contained in a <a href="http://git-scm.com/">git</a> repository located <a href=
-          "http://repo.or.cz/w/wmaker-crm.git">here</a>. Git is very convenient for our purposes and you can find more
-          information about it on the mentioned link. To obtain a full-fledged copy of the repository do this:<br /></p>
+          <p>The source code for Window Maker is contained in a <a href="http://git-scm.com/" target="_blank">git</a> repository located <a href=
+          "http://repo.or.cz/w/wmaker-crm.git" target="_blank">here</a>. To obtain a full-fledged copy of the repository do this:<br /></p>
 
           <p>git clone git://repo.or.cz/wmaker-crm.git<br /></p>
 
+	  <p> There are two main branches in the repository, called 'master' and 'next'. The purpose of the 'next' branch is to add
+	  and extra layer of testing before the patches hit the 'master' branch. It is rebased when needed. The 'master' branch
+	  should ideally never be rebased -- if it is, run to the nearest anti-nuclear bunker.</p>
+
           <h3>Submitting patches</h3>
 
-          <p>The source code in the above git repository follows the coding style of the linux kernel. Please take a look
-          <a href="http://git.kernel.org/?p=linux/kernel/git/torvalds/linux.git;a=blob_plain;f=Documentation/CodingStyle;hb=HEAD">
-          here</a> and try to respect it when submitting patches. This is really important.</p>
+          <p>The Window Maker source code follows the
+          <a href="http://git.kernel.org/?p=linux/kernel/git/torvalds/linux.git;a=blob_plain;f=Documentation/CodingStyle;hb=HEAD" target="_blank">
+          coding style of the linux kernel</a>. Respect it when submitting patches.</p>
 
-          <p>If you want to contribute patches to Window Maker and you are not familiar with git, please take a look at the git
-          homepage above - it contains the kind of documentation you need to get started. You should also read the file contained
+          <p>If you are not familiar with git, take a look at the <a href="http://git-scm.com/" target="_blank">git homepage</a>
+          -- it contains the kind of documentation you need to get started. You should also read the file contained
           in the Window Maker repository <a href=
-          "http://repo.or.cz/w/wmaker-crm.git/blob/HEAD:/The-perfect-Window-Maker-patch.txt">The perfect Window Maker patch</a>
+          "http://repo.or.cz/w/wmaker-crm.git/blob/HEAD:/The-perfect-Window-Maker-patch.txt" target="_blank">The perfect Window Maker patch</a>
           which gives you further details about patches to Window Maker.</p>
 
-          <p>If your patch is really good and important, feel free to send it to the mailing list
+          <p>If your patch is really good and important, feel free to send it to the mailing list at
           wmaker-dev@lists.windowmaker.org anyway in whatever form most suits you. But please consider the fact that patches sent
           in odd formats induce extra work for who applies them.</p>
 
-          <p>Last but not least, patches doing code cleanups are <strong>STRONGLY</strong> encouraged. Please go ahead! The risks
-          of introducing regressions when doing cleanups exists, but if you also follow the practice of having a patch for each
-          unrelated change you do, the eventuall regressions can be easily found and reverted.</p>
+          <p>Last but not least, patches doing code cleanups are <strong>STRONGLY</strong> encouraged.</p>
 
           <h3>Git repository for dockapps</h3>
 
-          <p>There is also a <a href="http://repo.or.cz/w/dockapps.git">git repository</a> containing a few dockapps which
+          <p>There is also a <a href="http://repo.or.cz/w/dockapps.git" target="_blank">git repository</a> containing a few dockapps which
           apparently have no maintainers anymore. Patches for those dockapps (or to include more apps) can also be sent to
           wmaker-dev@lists.windowmaker.org.</p>
 
@@ -59,7 +60,7 @@
             </li>
 
             <li>
-              <p><a href="http://tronche.com/gui/x/xlib/">The Xlib Manual</a></p>
+              <p><a href="http://tronche.com/gui/x/xlib/" target="_blank">The Xlib Manual</a></p>
             </li>
           </ul><br />
           <br />
