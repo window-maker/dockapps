b1aa9de48964d4c7831ed57ceec2caae9d3efd70
Carlos R. Mafra <crmafra@gmail.com>
2012-02-11 18:00:27 +0000

    A few more links

--- links.php
+++ links.php
@@ -22,36 +22,45 @@
 
           <ul>
             <li>
-              <p><a href="http://www.rumbero.org/wmlive">WMLive CD</a> -- A remastered Ubuntu live CD featuring Window Maker as 
+              <p><a href="http://www.rumbero.org/wmlive" target="_blank">WMLive CD</a> -- A remastered Ubuntu live CD featuring Window Maker as 
               default window manager.</p>
             </li>
 
 	   <li>
-	      <p><a href="http://www.quantitativefinanceservices.com/OpenDir/WINGslib/WINGToc.html">WINGs tutorial</a></p>
+	      <p><a href="http://www.quantitativefinanceservices.com/OpenDir/WINGslib/WINGToc.html" target="_blank">WINGs tutorial</a></p>
 	    </li>
 
             <li>
-              <p> <a href="http://www.the-little-red-haired-girl.org/pub/wmaker/"> Unofficial debian packages of Window Maker</a></p>
+              <p> <a href="http://www.the-little-red-haired-girl.org/pub/wmaker/" target="_blank"> Unofficial debian packages of Window Maker</a></p>
             </li>
 
-            <li>
-              <p><a href="http://administratosphere.wordpress.com/2011/07/05/why-i-use-window-maker/">Why I use Window Maker</a></p>
+	    <li>
+	      <p><a href="http://www.jessanderson.org/wmthemes/" target="_blank">Themes for Window Maker</a></p>
+	    </li>
+
+	    <li>
+	      <p><a href="http://voins.program.ru/windowmaker/wingsman.html" target="_blank">WINGs manpages</a></p>
+	    </li>
+
+	    <li>
+	      <p><a href="http://voins.program.ru/wdm/" target="_blank">WDM</a> -- A login manager</p>
+	    </li>
+
+	    <li>
+	      <p><a href="http://repo.or.cz/w/whome.git" target="_blank">Source code</a> of www.windowmaker.org -- Patches welcome</p>
+	    </li>
+
+	    <li>
+	      <p><a href="http://www.youtube.com/watch?v=WyxByfhT1F0&list=FL1YOrvHiWO_kPjkKGhO6vDA&feature=mh_lolz" target="_blank">OPENSTEP 4.0 Demo</a> on youtube</p>
+	    </li>
+
+	    <li>
+              <p><a href="http://administratosphere.wordpress.com/2011/07/05/why-i-use-window-maker/" target="_blank">Why I use Window Maker</a> (blog post)</p>
             </li>
-	    <li>
-	      <p><a href="http://www.jessanderson.org/wmthemes/">Themes for Window Maker</a></p>
-	    </li>
 
 	    <li>
-	      <p><a href="http://voins.program.ru/windowmaker/wingsman.html">WINGs manpages</a></p>
-	    </li>
-
-	<li>
-	      <p><a href="http://voins.program.ru/wdm/">WDM</a> -- A login manager</p>
-	    </li>
-
-	    <li>
-	      <p><a href="http://repo.or.cz/w/whome.git">Source code</a> of www.windowmaker.org -- Patches welcome</p>
-	    </li>
+              <p><a href="http://blog.bigsmoke.us/2009/03/30/wmaker" target="_blank">My Window Maker 'desktop' configuration</a> (blog post)</p>
+            </li>
 
 	<br><br><br>
           </ul>
