9700cde064048035037121f22d5e042a251867e3
Carlos R. Mafra <crmafra@gmail.com>
2012-02-14 19:19:34 +0000

    Remove hardcoded http link

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
     <table width="100%">
       <tr>
         <td width="100%">
-          <a href="http://beta.windowmaker.org/index.php"><img src="header.png" style="min-width:700px" width="95%" alt="title bar"></a>
+          <a href="index.php"><img src="header.png" style="min-width:700px" width="95%" alt="title bar"></a>
       </tr>
       <tr>
         <td width="100%">
