117a2451c86964ded636d5741e713ad056e5d08b
Doug Torrance <dtorrance@monmouthcollege.edu>
2014-08-10 01:50:34 -0500

    dock.php, header.php: Update include files to use absolute paths.
    The links and images used in the dock.php header.php include files use relative
    paths.  This poses a problem if someone were to include them from a file in a
    subdirectory.  In particular, the links and images used by the include files
    would be assumed to be in the same subdirectory.
    
    This patch fixes this issue by using absolute paths instead.

--- dock.php
+++ dock.php
@@ -1,36 +1,37 @@
+<?php $root = "http://www.windowmaker.org/";?>
   <div id="dock">
     <table width="64" height="500" border="0" cellpadding="0" cellspacing="0">
       <tr>
-        <td><a href="home.php"><img src="homewmaker.png" alt="Home" width="60" height="81"></a></td>
+        <td><a href="<?php echo $root;?>home.php"><img src="<?php echo $root;?>homewmaker.png" alt="Home" width="60" height="81"></a></td>
       </tr>
 
       <tr>
-        <td><a href="news.php"><img src=
-        "news.png" alt="News" width="60" height="81"></a></td>
+        <td><a href="<?php echo $root;?>news.php"><img src=
+        "<?php echo $root;?>news.png" alt="News" width="60" height="81"></a></td>
       </tr>
 
       <tr>
-        <td><a href="docs.php"><img src=
-        "docs.png" alt="Documentation" width="60" height="81"></a></td>
+        <td><a href="<?php echo $root;?>docs.php"><img src=
+        "<?php echo $root;?>docs.png" alt="Documentation" width="60" height="81"></a></td>
       </tr>
 
       <tr>
-        <td><a href="lists.php"><img src="mailing_list.png" alt="Mailing Lists" width="60" height=
+        <td><a href="<?php echo $root;?>lists.php"><img src="<?php echo $root;?>mailing_list.png" alt="Mailing Lists" width="60" height=
         "81"></a></td>
       </tr>
 
       <tr>
-        <td><a href="dev.php"><img src="development.png" alt="Development" width="60" height=
+        <td><a href="<?php echo $root;?>dev.php"><img src="<?php echo $root;?>development.png" alt="Development" width="60" height=
         "81"></a></td>
       </tr>
 
       <tr>
-        <td><a href="themes.php"><img src="themes.png" alt="Themes" width="60" height=
+        <td><a href="<?php echo $root;?>themes.php"><img src="<?php echo $root;?>themes.png" alt="Themes" width="60" height=
         "81"></a></td>
       </tr>
 
       <tr>
-        <td><a href="links.php"><img src="links.png" alt="Links" width="60" height=
+        <td><a href="<?php echo $root;?>links.php"><img src="<?php echo $root;?>links.png" alt="Links" width="60" height=
         "81"></a></td>
       </tr>
     </table>
--- header.php
+++ header.php
@@ -2,7 +2,7 @@
     <table width="100%">
       <tr>
         <td width="100%">
-          <a href="index.php"><img src="header.png" style="min-width:700px" width="95%" alt="title bar"></a>
+          <a href="<?php echo $root;?>"><img src="<?php echo $root;?>header.png" style="min-width:700px" width="95%" alt="title bar"></a>
       </tr>
       <tr>
         <td width="100%">
