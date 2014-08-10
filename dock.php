b9ef3a23a82863bc71e2dc5f50cda2891660a458
Doug Torrance <dtorrance@monmouthcollege.edu>
2014-08-10 01:50:33 -0500

    dock.php: Remove unused onmouseout and onmouseover attributes.
    The onmouseout and onmouseover attributes to the <a> tags in dock.php called
    javascript functions MM_swapImage() and MM_swapImgRestore(), which appear
    nowhere else on the website.  Therefore, they have been removed.

--- dock.php
+++ dock.php
@@ -1,41 +1,36 @@
   <div id="dock">
     <table width="64" height="500" border="0" cellpadding="0" cellspacing="0">
       <tr>
-        <td><a href="home.php" onmouseout="MM_swapImgRestore()" onmouseover=
-        "MM_swapImage('Image1','','homewmaker.png',0)"><img src="homewmaker.png" alt="Home" width="60" height="81"></a></td>
+        <td><a href="home.php"><img src="homewmaker.png" alt="Home" width="60" height="81"></a></td>
       </tr>
 
       <tr>
-        <td><a href="news.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image2','','news.png',0)"><img src=
+        <td><a href="news.php"><img src=
         "news.png" alt="News" width="60" height="81"></a></td>
       </tr>
 
       <tr>
-        <td><a href="docs.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image3','','docs.png',0)"><img src=
+        <td><a href="docs.php"><img src=
         "docs.png" alt="Documentation" width="60" height="81"></a></td>
       </tr>
 
       <tr>
-        <td><a href="lists.php" onmouseout="MM_swapImgRestore()" onmouseover=
-        "MM_swapImage('Image4','','mailing_list.png',0)"><img src="mailing_list.png" alt="Mailing Lists" width="60" height=
+        <td><a href="lists.php"><img src="mailing_list.png" alt="Mailing Lists" width="60" height=
         "81"></a></td>
       </tr>
 
       <tr>
-        <td><a href="dev.php" onmouseout="MM_swapImgRestore()" onmouseover=
-        "MM_swapImage('Image5','','development.png',0)"><img src="development.png" alt="Development" width="60" height=
+        <td><a href="dev.php"><img src="development.png" alt="Development" width="60" height=
         "81"></a></td>
       </tr>
 
       <tr>
-        <td><a href="themes.php" onmouseout="MM_swapImgRestore()" onmouseover=
-        "MM_swapImage('Image6','','themes.png',0)"><img src="themes.png" alt="Themes" width="60" height=
+        <td><a href="themes.php"><img src="themes.png" alt="Themes" width="60" height=
         "81"></a></td>
       </tr>
 
       <tr>
-        <td><a href="links.php" onmouseout="MM_swapImgRestore()" onmouseover=
-        "MM_swapImage('Image7','','links.png',0)"><img src="links.png" alt="Links" width="60" height=
+        <td><a href="links.php"><img src="links.png" alt="Links" width="60" height=
         "81"></a></td>
       </tr>
     </table>
