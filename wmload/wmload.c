7cc19cb1ccaabcff43f99c082e8a19ad4967ff5d
Doug Torrance <dtorrance@monmouthcollege.edu>
2015-04-15 09:19:40 -0500

    wmload: Clarify copyright information.
    Add copyright header to wmload.c and COPYING file, obtained from
    https://www.gnu.org/licenses/gpl-2.0.txt.

--- wmload/wmload.c
+++ wmload/wmload.c
@@ -1,3 +1,23 @@
+/* wmload - system load monitor designed for Window Maker
+*  Copyright (C) 1996 Beat Christen <bchriste@iiic.ethz.ch>
+*  Copyright (C) 1997 Ryan Land <rland@bc1.com>
+*  Copyright (C) 2015 Window Maker Developers Team
+*                     <wmaker-dev@lists.windowmaker.org>
+*
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License along
+*  with this program; if not, write to the Free Software Foundation, Inc.,
+*  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
+
 #define _POSIX_C_SOURCE 199309L
 
 #include <stdio.h>
