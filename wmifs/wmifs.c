90565e05d5cae36cda6e4a1be43516ba7cd698c8
Doug Torrance <dtorrance@monmouthcollege.edu>
2015-05-27 17:33:17 -0500

    wmifs: Replace deprecated caddr_t with void *.

--- wmifs/wmifs.c
+++ wmifs/wmifs.c
@@ -957,7 +957,7 @@ void get_ppp_stats(struct ppp_stats *cur)
 
 	memset(&req, 0, sizeof(req));
 
-	req.stats_ptr = (caddr_t) &req.stats;
+	req.stats_ptr = (void *) &req.stats;
 
 	sprintf(req.ifr__name, "ppp%d", PPP_UNIT);
 
