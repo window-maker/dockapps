f35a87796f8f241c4753c4d24e6700358bda8e70
Rodolfo García Peñas (kix) <kix@kix.es>
2015-08-15 10:38:19 +0200

    wmmon uses libdockapp
    The dockapp wmmon uses the new version of libdockapp and do not use
    libwmgeneral.
    
    Signed-off-by: Rodolfo García Peñas (kix) <kix@kix.es>

--- wmmon/wmmon/wmmon.c
+++ wmmon/wmmon/wmmon.c
@@ -108,8 +108,8 @@
 #include <X11/xpm.h>
 #include <X11/extensions/shape.h>
 
-#include <wmgeneral/wmgeneral.h>
-#include <wmgeneral/misc.h>
+#include <libdockapp/wmgeneral.h>
+#include <libdockapp/misc.h>
 
 #include "wmmon-master.xpm"
 #include "wmmon-mask.xbm"
