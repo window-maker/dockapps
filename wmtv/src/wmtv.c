00b14d1a67e09c7bcc4eead89a5440a9dd174600
Doug Torrance <dtorrance@piedmont.edu>
2016-02-01 00:45:18 -0500

    wmtv: Replace obsolete include of xf86dga.h with Xxf86dga.h.
    Fixes the following warning:
       In file included from src/wmtv.c:47:0:
       /usr/include/X11/extensions/xf86dga.h:9:2: warning: #warning "xf86dga.h is
         obsolete and may be removed in the future." [-Wcpp]
        #warning "xf86dga.h is obsolete and may be removed in the future."
         ^
       /usr/include/X11/extensions/xf86dga.h:10:2: warning: #warning "include
         <X11/extensions/Xxf86dga.h> instead." [-Wcpp]
        #warning "include <X11/extensions/Xxf86dga.h> instead."
         ^

--- wmtv/src/wmtv.c
+++ wmtv/src/wmtv.c
@@ -44,7 +44,7 @@
 #include <X11/keysym.h>
 #include <X11/xpm.h>
 #include <X11/extensions/shape.h>
-#include <X11/extensions/xf86dga.h>
+#include <X11/extensions/Xxf86dga.h>
 #include <X11/extensions/xf86vmode.h>
 
 #include <libdockapp/wmgeneral.h>
