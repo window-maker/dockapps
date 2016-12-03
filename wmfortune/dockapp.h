5eab1dd9f3c2eb9f2a316af07b82fa1845856832
Doug Torrance <dtorrance@piedmont.edu>
2016-12-03 16:49:52 -0500

    wmfortune: Add version 0.241 to repository.
    From
    http://gentoo.osuosl.org/distfiles/wmfortune-0.241.tar.gz

---
+++ wmfortune/dockapp.h
@@ -0,0 +1,230 @@
+/*
+ * 
+ * Copyright (c) 1999 Alfredo K. Kojima
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ */
+
+#ifndef _DOCKAPP_H_
+#define _DOCKAPP_H_
+
+
+/*
+ * This is a simple (trivial) library for writing Window Maker dock 
+ * applications, or dockapps (those that only show up in the dock), easily.
+ * 
+ * It is very limited and can be only used for dockapps that open a single
+ * appicon for process in only one single display, but this seems to be
+ * enough for most, if not all, dockapps.
+ */
+
+
+#include <X11/Xlib.h>
+#include <X11/xpm.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+extern Display *DADisplay;
+
+extern Window DAWindow;
+
+extern int DADepth;
+
+extern Visual *DAVisual;
+
+
+/* the callbacks for events related to the dockapp window your program wants
+ * to handle */
+typedef struct {
+    /* the dockapp window was destroyed */
+    void (*destroy)();
+    /* button pressed */
+    void (*buttonPress)(int button, int state, int x, int y);
+    /* button released */
+    void (*buttonRelease)(int button, int state, int x, int y);
+    /* pointer motion */
+    void (*motion)(int x, int y);
+    /* pointer entered dockapp window */
+    void (*enter)();
+    /* pointer leaved dockapp window */
+    void (*leave)();
+    /* timer expired */
+    void (*timeout)();
+} DACallbacks;
+
+
+/* option argument types */
+enum {
+    DONone,			       /* simple on/off flag */
+	DOInteger,		       /* an integer number */
+	DOString,		       /* a string */
+	DONatural		       /* positive integer number */
+};
+
+
+typedef struct {
+    char *shortForm;		       /* short form for option, like -w */
+    char *longForm;		       /* long form for option, like --withdrawn */
+    char *description;		       /* description for the option */
+
+    short type;			       /* type of argument */
+
+    Bool used;			       /* if the argument was passed in the 
+					cmd line */
+    /* the following are only set if the "used" field is True */
+    union {			       /* a ptr for the value that was passed
+					in the command line */
+	void *ptr;
+
+	int *integer;
+
+	char **string;
+    } value;
+} DAProgramOption;
+
+
+
+/*
+ * DAParseArguments-
+ * 	Command line argument parser. The program is exited if there are
+ * syntax errors.
+ *
+ * -h, --help and --version are automatically handled (causing the program
+ * to exit)
+ * 
+ */
+void DAParseArguments(int argc, char **argv, DAProgramOption *options,
+		      int count, char *programDescription,
+		      char *versionDescription);
+
+
+/*
+ * DAInitialize-
+ * 	Initialize the dockapp, open a connection to the X server,
+ * create the needed windows and setup them to become an appicon window.
+ * It will automatically detect if Window Maker is present and use
+ * an appropriate form form
+ *
+ * You must call this always before calling anything else (except for
+ * DAParseArguments())
+ *
+ * Arguments:
+ * 	display - the name of the display to connect to. Use "" to use the
+ * 		default value
+ * 	name - the name of your dockapp, used as the class name for
+ *		the WM_CLASS hint. Like WMYAClock
+ * 	width, height - the size of the dockapp window. 48x48 is the 
+ *		preferred size
+ * 	argc, argv - the program arguments. argv[0] will be used as the
+ * 		instance name for the WM_CLASS hint.
+ */
+void DAInitialize(char *display, char *name, unsigned width, unsigned height,
+		  int argc, char **argv);
+
+/*
+ * DASetShape-  
+ * 	Sets the shape mask of the dockapp to the specified one. This is
+ * optional. If you pass None as shapeMask, the dockapp will become
+ * non-shaped.
+ *
+ * This is only needed if you want the dockapp to be shaped.
+ */
+void DASetShape(Pixmap shapeMask);
+
+/*
+ * DASetPixmap-
+ * 	Sets the image pixmap for the dockapp. Once you set the image with
+ * it, you don't need to handle expose events.
+ */
+void DASetPixmap(Pixmap pixmap);
+
+/*
+ * DAMakePixmap-
+ * 	Creates a pixmap suitable for using with DASetPixmap()
+ */
+Pixmap DAMakePixmap();
+
+
+
+/*
+ * DAMakePixmapFromData-
+ * 	Creates a pixmap and mask from XPM data
+ */
+Bool DAMakePixmapFromData(char **data, Pixmap *pixmap, Pixmap *mask,
+			  unsigned *width, unsigned *height);
+
+
+/*
+ * Returns a color.
+ */
+unsigned long DAGetColor(char *colorName);
+/*
+ * DAShow-
+ * 	Opens the dockapp.
+ *
+ * Always call this function or the dockapp won't show up.
+ */
+void DAShow();
+
+/*
+ * DASetCallbacks-
+ * 	Register a set of callbacks for events like mouse clicks.
+ * 
+ * Only needed if you want to receive some event.
+ */
+void DASetCallbacks(DACallbacks *callbacks);
+
+/*
+ * DASetTimeout-
+ * 	Sets a timeout for the DAEventLoop(). The timeout callback
+ * will be called whenever the app doens't get any events from the
+ * X server in the specified time.
+ */
+void DASetTimeout(int milliseconds);
+
+
+/*
+ * DANextEventOrTimeout-
+ * 	Waits until an event is received or the timeout limit is
+ * expired. Returns True if an event was received.
+ */
+Bool DANextEventOrTimeout(XEvent *event, unsigned long millisec);
+
+
+/*
+ * DAProcessEvent-
+ * 	Processes an event. Returns True if the event was handled and
+ * False otherwise.
+ * 
+ * Must be called from your event loop, unless you use DAEventLoop()
+ */
+Bool DAProcessEvent(XEvent *event);
+
+/*
+ * DAEventLoop-
+ * 	Enters an event loop where events are processed until the dockapp
+ * is closed. This function never returns.
+ */
+void DAEventLoop();
+
+
+
+#endif
---
+++ wmfortune/wmfortune.c
@@ -0,0 +1,222 @@
+/*
+ * $Id: wmfortune.c,v 0.24 2000/03/28 01:49:58 sugano Exp $
+ * wmfortune Copyright (c) 2000 Makoto Sugano
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+#define BUFF_SIZE 1024
+#define SCROLL_DEFAULT_SPEED 1000
+
+#include <unistd.h>
+#include <stdio.h>
+#include "dockapp.h"
+#include "XPM/panel.xpm"
+#include "XPM/panel_button_pressed.xpm"
+#include "XPM/panel_window.xpm"
+
+/* WINDOW: window for showing the messages */
+#define WINDOW_X 4
+#define WINDOW_Y 4
+#define WINDOW_WIDTH 56
+#define WINDOW_HEIGHT 22
+
+/* BUTTON: button for the new fortune message */
+#define BUTTON_X 6
+#define BUTTON_Y 32
+#define BUTTON_WIDTH 52
+#define BUTTON_HEIGHT 28
+
+/* 6 pixels are used to draw a letter */
+#define PIXELS_PER_LETTER 6
+
+#define STRING_Y 18
+
+/*
+ * pixel lag between the last letter of the previous message
+ * and the first letter of the next message.
+ * if set to 0, the messages appear quickly after
+ * the previous messages ends.
+ */
+#define MESSAGE_LAG 5
+
+char *displayName = "";
+int speed = SCROLL_DEFAULT_SPEED;
+
+/* global variables for main() and callbacks */
+FILE *fortune;
+Pixmap pixmap;
+Pixmap panel_button_pressed_pixmap;
+Pixmap panel_pixmap, panel_mask;
+GC gc;
+int c;
+char buff[BUFF_SIZE];
+
+/* 
+ * i: used to count the number of letters in fill_buff().
+ * j: the number that points the message starting pixel.
+ */
+int i, j;
+
+/* fill the buffer with the fortune message. */
+static void
+fill_buff (void)
+{
+  i = 0;
+
+  memset(buff, '\0', BUFF_SIZE);
+  if (!(fortune = popen("fortune -s", "r")))
+    {
+      perror("fortune");
+      exit(0);
+    }
+
+  while (( c = fgetc(fortune)) != EOF)
+    {
+      if (c == '\n')
+	{
+	  buff[i++] = ' ';
+	}
+      else if (c == '\t')
+	{
+	  buff[i++] = ' ';
+	  buff[i++] = ' ';
+	  buff[i++] = ' ';
+	}
+      else
+	{
+	  buff[i++] = c;
+	}
+    }
+
+  j = i + MESSAGE_LAG;
+  pclose(fortune);
+}
+
+static DAProgramOption options[] = {
+  {"-d", "--displayname", "display to use.", DOString, False, {&displayName}},
+  {"-s", "--speed", "scrolling speed. (default 1000 dot/ms)", DOInteger, False, {&speed}},
+};
+
+static void
+buttonReleaseCallback (int button, int state, int x, int y)
+{
+  if (button == 1 && BUTTON_X < x &&
+      x < (BUTTON_X + BUTTON_WIDTH) &&
+      BUTTON_Y < y && y < (BUTTON_Y + BUTTON_HEIGHT))
+    {
+      fill_buff();      
+    }
+  XCopyArea(DADisplay, panel_pixmap, pixmap, gc,
+	    BUTTON_X, BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT,
+	    BUTTON_X, BUTTON_Y);
+}
+
+static void
+buttonPressCallback (int button, int state, int x, int y)
+{
+  if (button == 1 && BUTTON_X < x &&
+      x < (BUTTON_X + BUTTON_WIDTH) &&
+      BUTTON_Y < y && y < (BUTTON_Y + BUTTON_HEIGHT))
+    {
+      XCopyArea(DADisplay, panel_button_pressed_pixmap, pixmap, gc,
+		BUTTON_X, BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT,
+		BUTTON_X, BUTTON_Y);
+    }
+}
+
+int
+main(int argc, char **argv)
+{
+   Pixmap panel_window_pixmap;
+
+   int w = 64, h = 64;
+
+   DACallbacks callbacks = {NULL,buttonPressCallback
+	,buttonReleaseCallback,NULL,NULL,NULL,NULL};
+   
+   DAParseArguments(argc, argv, options,
+		    sizeof(options)/sizeof(DAProgramOption),
+		    "dockapp that shows the messages from fortune command.", "$Id: wmfortune.c,v 0.24 2000/03/28 01:49:58 sugano Exp $");
+
+   fill_buff();      
+
+   DAInitialize(displayName, "wmfortune", 64, 64, argc, argv);   
+   pixmap = DAMakePixmap();
+   
+   /* making pixmap for the panel */
+   DAMakePixmapFromData(panel_xpm, &panel_pixmap,
+			&panel_mask, &w, &h);
+   DAMakePixmapFromData(panel_button_pressed_xpm,
+			&panel_button_pressed_pixmap, NULL, &w, &h);
+   DAMakePixmapFromData(panel_window_xpm,
+			&panel_window_pixmap, NULL, &w, &h);
+
+   /* setting up the mask for the panel */
+   DASetShape(panel_mask);
+   DASetPixmap(panel_pixmap);
+
+   /* setting up the graphic context */
+   gc = DefaultGC(DADisplay, DefaultScreen(DADisplay));
+
+   DASetCallbacks(&callbacks);
+   DAShow();
+
+   /* drawing the button */
+   XCopyArea(DADisplay, panel_pixmap, pixmap, gc,
+	     BUTTON_X, BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT,
+	     BUTTON_X, BUTTON_Y);
+   
+   while (1)
+     {
+       XEvent ev;
+       
+       /* sets the foreground color green */
+       XSetForeground(DADisplay, gc, DAGetColor("green"));
+       XCopyArea(DADisplay, panel_window_pixmap, pixmap, gc,
+		 WINDOW_X, WINDOW_Y, WINDOW_WIDTH, WINDOW_HEIGHT,
+		 WINDOW_X, WINDOW_Y);
+       XDrawString(DADisplay, pixmap, gc, j, STRING_Y, buff, strlen(buff));
+       
+       DASetPixmap(pixmap);
+
+       /* scroll the message by a pixel to left */
+       i--;
+       j = i + MESSAGE_LAG;
+       
+       /*
+	* starts scrolling the messages from the beginning
+	* if the message ends.
+        */
+       if (j == - (PIXELS_PER_LETTER) * strlen(buff))
+	 {
+	   /* if not 64, message suddenly appears in the window */
+	   i = 64;
+	   j = i + MESSAGE_LAG;
+	 }
+
+       /* handle all pending X events */
+       while (XPending(DADisplay))
+	 {
+	   XNextEvent(DADisplay, &ev);
+	   DAProcessEvent(&ev);
+	 }
+       usleep(speed);
+     }
+   return 0;
+}
