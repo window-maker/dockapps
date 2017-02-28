efb171a5dc86e101c53f88d7795ea646a1b984b8
Doug Torrance <dtorrance@piedmont.edu>
2017-02-27 20:50:46 -0500

    cnslock: Bump to version 1.03.

--- cnslock/cnslock.c
+++ cnslock/cnslock.c
@@ -1,5 +1,6 @@
-/* csnlock v1.02
+/* cnslock
  * Copyright (C) 2002 Simon Hunter (lists@sprig.dyn.dhs.org)
+ * Copyright (C) 2017 Window Maker Team (wmaker-dev@lists.windowmaker.org)
  *
  * cnslock is a dock application that displays the current state of the
  * three lock keys (caps, num, and scroll)
