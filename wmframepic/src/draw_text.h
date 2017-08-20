c93e4e8bd9e6bb2679070ae27c92b15585526eb9
Doug Torrance <dtorrance@piedmont.edu>
2017-08-20 10:36:30 -0400

    wmframepic: Add includes and declarations to avoid implicit declarations.

--- wmframepic/src/draw_text.h
+++ wmframepic/src/draw_text.h
@@ -1,11 +1,14 @@
 #ifndef DRAW_TEXT_H_
 #define DRAW_TEXT_H_
 
+#include <ctype.h>
 #include <X11/Xlib.h>
+#include <X11/xpm.h>
 #include <string.h>
 #include "font.xpm"
 #include "display_text.xpm"
 
+void draw_text(char *text, int dx, int dy, Bool digit);
 void restore_background(void);
 void flush_background(void);
 void init_variables(Display	*g_display, GC g_gc, Window	g_icon_window, int	offset_w, int offset_h, Pixmap **background);
